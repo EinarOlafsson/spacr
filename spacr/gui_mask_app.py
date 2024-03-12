bc0ded2843f69b0f037715fdbef7153d76ea6921
EinarOlafsson <einar.olafsson@gmail.com>
2024-03-12 14:11:10 -0400

    bug fix

--- spacr/gui_mask_app.py
+++ spacr/gui_mask_app.py
@@ -205,29 +205,29 @@ def initiate_mask_root(width, height):
     
     return root, vars_dict
 
-#def gui_mask():
-#    global vars_dict, root
-#    root, vars_dict = initiate_mask_root(1000, 1500)
-#    root.mainloop()
-    
 def gui_mask():
-    from .cli import get_arg_parser
-    from .version import version_str
-
-    args = get_arg_parser().parse_args()
-    
-    if args.version:
-        print(version_str)
-        return
-
-    if args.headless:
-        settings = {}
-        spacr.core.preprocess_generate_masks(settings['src'], settings=settings, advanced_settings={})
-        return
-
     global vars_dict, root
     root, vars_dict = initiate_mask_root(1000, 1500)
     root.mainloop()
+    
+#def gui_mask():
+#    from .cli import get_arg_parser
+#    from .version import version_str
+#
+#    args = get_arg_parser().parse_args()
+#    
+#    if args.version:
+#        print(version_str)
+#        return
+#
+#    if args.headless:
+#        settings = {}
+#        spacr.core.preprocess_generate_masks(settings['src'], settings=settings, advanced_settings={})
+#        return
+#
+#    global vars_dict, root
+#    root, vars_dict = initiate_mask_root(1000, 1500)
+#    root.mainloop()
 
 if __name__ == "__main__":
     gui_mask()
