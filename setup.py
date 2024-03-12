c77d6cefb6c3fddd76a328ef75934c3a88c5bad7
EinarOlafsson <einar.olafsson@gmail.com>
2024-03-12 17:49:31 -0400

    bug fix

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ dependencies = [
 
 setup(
     name="spacr",
-    version="0.0.17",
+    version="0.0.18",
     author="Einar Birnir Olafsson",
     author_email="olafsson@med.umich.com",
     description="Spatial phenotype analysis of crisp screens (SpaCr)",
