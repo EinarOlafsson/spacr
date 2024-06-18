671ee9820f63ad544bb5149c37d68c42668f2aa4
Einar work <einar.olafsson@gmail.com>
2024-06-18 18:34:15 -0400

    Set up Sphinx documentation with Read the Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../spacr'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 project = 'spacr'
@@ -12,7 +12,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
