0df5d837cfdfa4ed0bbacce71916175d95fe22e4
Einar work <einar.olafsson@gmail.com>
2024-06-18 13:59:33 -0400

    Update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,16 +1,11 @@
 import os
 import sys
-
-# Insert the package path into sys.path
 sys.path.insert(0, os.path.abspath('../..'))
 
-# -- Project information -----------------------------------------------------
 project = 'spacr'
-copyright = '2024, Einar Birnir Olafsson'
 author = 'Einar Birnir Olafsson'
 release = '0.0.70'
 
-# -- General configuration ---------------------------------------------------
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
@@ -20,6 +15,6 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
