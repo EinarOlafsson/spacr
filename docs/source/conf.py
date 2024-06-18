3779cfb802dffd6eb865059b81329ad05835460e
Einar work <einar.olafsson@gmail.com>
2024-06-18 16:10:56 -0400

    Add updated documentation using Read the Docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+import sphinx_rtd_theme
+
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information -----------------------------------------------------
@@ -6,23 +8,20 @@ author = 'Einar Birnir Olafsson'
 release = '0.0.70'
 
 # -- General configuration ---------------------------------------------------
-# Add any Sphinx extension module names here, as strings.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
 
-# The master toctree document.
-master_doc = 'index'
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
-
 templates_path = ['_templates']
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
+
+# Specify the master document
+master_doc = 'index'
 
