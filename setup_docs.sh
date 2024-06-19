ce61977126a1fb0ff0a5b8a737d0fe676ab6e757
Einar work <einar.olafsson@gmail.com>
2024-06-19 11:34:48 -0400

    Documentation

--- setup_docs.sh
+++ setup_docs.sh
@@ -6,8 +6,8 @@ pip install sphinx sphinx_rtd_theme sphinx-autodoc-typehints
 # Remove existing docs directory if it exists
 rm -rf docs
 
-# Create the docs directory
-mkdir -p docs
+# Create the docs directory and source subdirectory
+mkdir -p docs/source
 cd docs
 
 # Create docs/requirements.txt before initializing Sphinx
@@ -15,11 +15,32 @@ cat <<EOL > requirements.txt
 sphinx
 sphinx_rtd_theme
 sphinx-autodoc-typehints
--e ..
 EOL
 
-# Run sphinx-quickstart with necessary configurations
-sphinx-quickstart -q -p "spacr" -a "Your Name" -v "0.1" --ext-autodoc --ext-viewcode --ext-todo .
+# Create the .readthedocs.yaml configuration file
+cat <<EOL > ../.readthedocs.yaml
+version: 2
+
+build:
+  os: ubuntu-20.04
+  tools:
+    python: "3.9"
+
+sphinx:
+  configuration: docs/source/conf.py
+
+python:
+  install:
+    - requirements: docs/requirements.txt
+    - method: pip
+      path: .
+EOL
+
+# Get the version of spacr from the version.py file
+VERSION=$(python -c "from spacr.version import version; print(version)")
+
+# Run sphinx-quickstart with necessary configurations in the source directory
+sphinx-quickstart source -q -p "spacr" -a "Your Name" -v "$VERSION" --ext-autodoc --ext-viewcode --ext-todo
 
 # Update conf.py with additional configurations
 cat <<EOT >> source/conf.py
@@ -45,11 +66,9 @@ html_static_path = ['_static']
 EOT
 
 # Create an index.rst file with basic content
-cat <<EOT >> source/index.rst
+cat <<EOT > source/index.rst
 .. spacr documentation master file, created by
-   sphinx-quickstart on Sat Sep 18 2021.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+   sphinx-quickstart on $(date).
 
 Welcome to spacr's documentation!
 =================================
@@ -66,19 +85,26 @@ Indices and tables
 * :ref:\`genindex\`
 * :ref:\`modindex\`
 * :ref:\`search\`
-
 EOT
 
 # Run sphinx-apidoc to generate the reStructuredText files
 sphinx-apidoc -o source/ ../spacr
 
 # Build the HTML documentation
-make html
+make -C source html
 
 # Commit the changes to git
 cd ..
-git add docs
+git add docs .readthedocs.yaml
 git commit -m "Set up Sphinx documentation with Read the Docs"
 git push origin main
 
 echo "Setup complete. Push the changes to your repository and configure Read the Docs."
+echo "spacr version: $VERSION"
+
+# Instructions to build the documentation on Read the Docs
+# chmod +x setup_docs.sh make the script executable.
+# Run the script with ./setup_docs.sh
+# 1. Go to https://readthedocs.org/ and log in with GitHub account.
+# 2. Click on 'Import a Project'.
+# 3. Select your repository and click 'Next'.
