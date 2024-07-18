e06a13af5693b2fc9c349c339a8b093ce2bc4e3f
Einar work <einar.olafsson@gmail.com>
2024-07-18 17:28:21 -0400

    gui fix

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ dependencies = [
 
 setup(
     name="spacr",
-    version="0.1.0",
+    version="0.1.01",
     author="Einar Birnir Olafsson",
     author_email="olafsson@med.umich.com",
     description="Spatial phenotype analysis of crisp screens (SpaCr)",
