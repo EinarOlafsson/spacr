da2faced8aecc190d08c7e33b20e9c205bb11278
Einar work <einar.olafsson@gmail.com>
2024-11-15 16:05:57 -0500

    freq

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ dependencies = [
 
 setup(
     name="spacr",
-    version="0.3.65",
+    version="0.3.66",
     author="Einar Birnir Olafsson",
     author_email="olafsson@med.umich.com",
     description="Spatial phenotype analysis of crisp screens (SpaCr)",
