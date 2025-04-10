16a69e1bed69bb76810907635e7171afb04e6619
Einar work <einar.olafsson@gmail.com>
2025-04-10 15:31:13 -0400

    Option B: mock heavy imports for RTD build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,34 +1,50 @@
 import os
 import sys
+# make sure Sphinx can import your package
 sys.path.insert(0, os.path.abspath('../../spacr'))
 
-
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'spacr'
 copyright = '2025, Einar Birnir Olafsson'
 author = 'Einar Birnir Olafsson'
 release = '0.4.60'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Add any Sphinx extension module names here, as strings.
+extensions = [
+    'sphinx.ext.autodoc',    # core autodoc functionality
+    'sphinx.ext.napoleon',   # for Google- and NumPy‑style docstrings
+]
 
-extensions = []
+# Mock out heavy/compiled/3rd‑party modules so autodoc won't try to import them
+autodoc_mock_imports = [
+    'tables',
+    'h5py',
+    'wandb',
+    'huggingface_hub',
+    'openai',
+    'tqdm',
+    'ipywidgets',
+    'ipykernel',
+    'screeninfo',
+    'brokenaxes',
+    'gdown',
+    'rapidfuzz',
+    # add any other modules your code imports but you don't need to execute
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'English'
+# If your source files aren’t UTF‑8, change this:
+# source_encoding = 'utf-8-sig'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
-extensions = ['sphinx.ext.autodoc']
 html_static_path = ['_static']
+
+# -- Napoleon settings (optional) --------------------------------------------
+# If you use Google or NumPy style docstrings and want to tweak Napoleon:
+# napoleon_google_docstring = True
+# napoleon_numpy_docstring  = True
+# napoleon_include_init_with_doc = False
