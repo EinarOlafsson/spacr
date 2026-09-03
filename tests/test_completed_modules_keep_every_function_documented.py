b915c259eaf5fec10c3a6fcf83ec2417ba5018e3
Einar Olafsson <einar.olafsson@gmail.com>
2026-09-03 02:24:31 -0400

    Complete metadata resolution documentation

--- tests/test_completed_modules_keep_every_function_documented.py
+++ tests/test_completed_modules_keep_every_function_documented.py
@@ -63,6 +63,7 @@ FULLY_DOCUMENTED = (
     "spacr/macro.py",
     "spacr/mask_io.py",
     "spacr/measure_hooks.py",
+    "spacr/metadata_resolution.py",
     "spacr/merge_tables.py",
     "spacr/model_check.py",
     "spacr/normalization.py",
