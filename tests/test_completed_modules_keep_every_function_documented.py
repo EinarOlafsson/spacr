c1bd6ddea8fa52c2448c8675ac5a0368af64d6fa
Einar Olafsson <einar.olafsson@gmail.com>
2026-09-03 07:58:40 -0400

    Complete version function documentation

--- tests/test_completed_modules_keep_every_function_documented.py
+++ tests/test_completed_modules_keep_every_function_documented.py
@@ -170,6 +170,7 @@ FULLY_DOCUMENTED = (
     "spacr/umap_annotations.py",
     "spacr/umap_search.py",
     "spacr/updater.py",
+    "spacr/version.py",
     "spacr/volcano_style.py",
     "spacr/well_scope.py",
     "spacr/well_spec.py",
