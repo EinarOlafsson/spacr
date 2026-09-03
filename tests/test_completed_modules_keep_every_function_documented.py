120ddcc1fa2b604f8d204d540a6cf9f1503e99aa
Einar Olafsson <einar.olafsson@gmail.com>
2026-09-03 07:13:54 -0400

    Complete pipeline v2 function documentation

--- tests/test_completed_modules_keep_every_function_documented.py
+++ tests/test_completed_modules_keep_every_function_documented.py
@@ -112,6 +112,7 @@ FULLY_DOCUMENTED = (
     "spacr/parameter_sweep.py",
     "spacr/permutation_qc.py",
     "spacr/picture_settings.py",
+    "spacr/pipeline_v2.py",
     "spacr/plate_measurements.py",
     "spacr/plate_qc.py",
     "spacr/plot.py",
