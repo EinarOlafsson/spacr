81326f3902b8f28018dd0bdbd89a27ae95ba9588
Einar Olafsson <einar.olafsson@gmail.com>
2026-09-10 06:02:51 -0400

    tutorials: record that partial AnnData files reopen without features

--- tools/tutorials/tests/test_anndata_evidence.py
+++ tools/tutorials/tests/test_anndata_evidence.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import pytest
-import anndata
+anndata=pytest.importorskip('anndata')
 sys.path.insert(0,str(Path(__file__).resolve().parents[1]))
 from anndata_evidence import FIELD, LABELS, reference, verify, verify_file
 
